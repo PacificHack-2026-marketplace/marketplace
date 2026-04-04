b588d7425f3efa2ab6a90f1a59b5856c0db36ce9
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-04-04 11:07:55 -0700

    add html template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def send():
     return "hello world", 200
 
+@app.route('/')
+def index():
+    return render_template("index.html", message="hello world")
+
+
 if __name__ == '__main__':
     app.run()
+    url_for('static', filename='style.css')
+
