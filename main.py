c04a2a286a0665186e32639668b473d8607168dd
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2026-04-04 17:27:25 -0700

    add google oauth

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from db import init_db, insert_listing, Listing, get_listing, get_top_listings
+from authlib.integrations.flask_client import OAuth
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
 
 app = Flask(__name__)
 
+app.secret_key = os.getenv("SECRET_KEY")
+
+oauth = OAuth(app)
+
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_OAUTH_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
 @app.route('/api/listing', methods=['POST'])
 def listing_endpoint():
+    user = session.get('user')
+
     listing = Listing(
         title=request.form.get("title"),
         category=request.form.get("category"),
         price=float(request.form.get("price")),
-        user_name=request.form.get("user_name"),
-        contact_email=request.form.get("contact_email"),
-        contact_phone=request.form.get("contact_phone"),
+        contact_email=user['email'],
+        user_name=user['name'],
         location=request.form.get("location"),
         description=request.form.get("description"),
-        summary=request.form.get("summary"),
     )
     id = insert_listing(listing)
 
@@ -40,8 +60,27 @@ def base():
 
 @app.route('/listing/create')
 def create_listing():
+    user = session.get('user')
+
+    if user is None:
+        return redirect("/login", 302)
+
+    print(user)
     return render_template("create_listing.html")
 
+@app.route('/login')
+def login():
+    return google.authorize_redirect(redirect_uri='http://127.0.0.1:5000/api/oauth/callback')
+
+@app.route('/api/oauth/callback')
+def callback():
+    token = google.authorize_access_token()
+    user = token.get("userinfo")    # insecure btw
+
+    # user info
+    session['user'] = user
+
+    return redirect('/')
 
 """
 @app.route('/base')
